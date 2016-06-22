570469c957bef46a1734ece264e1e452d142fec4
Pierre Gilot <pigilot@microsoft.com>
2016-06-22 17:02:09 +0200

    adding index file

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ bot.add('/', dialog)
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
